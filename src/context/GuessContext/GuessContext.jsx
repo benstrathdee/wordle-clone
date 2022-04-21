e6db96c810102c1acf8d889f2edae8c02f30fdd9
Ben Strathdee <ben.strathdee@live.com.au>
2022-04-21 20:29:34 +1000

    cookie function import changed

--- src/context/GuessContext/GuessContext.jsx
+++ src/context/GuessContext/GuessContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState } from "react";
-import { getCookie } from "../../functions/gridFunctions";
+import { getCookie } from "../../functions/utilities";
 
 export const GuessContext = createContext();
 
