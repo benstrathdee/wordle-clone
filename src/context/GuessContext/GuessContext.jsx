d8a2da95007bb832b0d3be39eb4fe3dbaf38828e
Ben Strathdee <ben.strathdee@live.com.au>
2022-04-21 18:31:19 +1000

    finalised most state values

--- src/context/GuessContext/GuessContext.jsx
+++ src/context/GuessContext/GuessContext.jsx
@@ -1,4 +1,5 @@
 import { createContext, useState } from "react";
+import { getCookie } from "../../functions/gridFunctions";
 
 export const GuessContext = createContext();
 
@@ -6,17 +7,22 @@ export const GuessProvider = ({ children }) => {
 	// Get two state values from local storage if present,
 	// Set to blank if not present
 	const [currentGuess, setCurrentGuess] = useState(
-		localStorage.currentGuess ? JSON.parse(localStorage.currentGuess) : []
+		localStorage.currentGuess ? getCookie("currentGuess") : []
 	);
 	const [prevGuesses, setPrevGuesses] = useState(
-		localStorage.prevGuesses ? JSON.parse(localStorage.prevGuesses) : []
+		localStorage.prevGuesses ? getCookie("prevGuesses") : []
 	);
+
+	// Set on page load depending on dateCode
+	const [wordOfDay, setWordOfDay] = useState([]);
 
 	const data = {
 		currentGuess,
 		setCurrentGuess,
 		prevGuesses,
 		setPrevGuesses,
+		wordOfDay,
+		setWordOfDay,
 	};
 	return (
 		<GuessContext.Provider value={data}>{children}</GuessContext.Provider>
