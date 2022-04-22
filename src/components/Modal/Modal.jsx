ba08d19524c3bcd0fd8a07a80cfbc1e39794ac4d
Ben Strathdee <ben.strathdee@live.com.au>
2022-04-22 18:00:02 +1000

    implemented basic modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -3,14 +3,19 @@ import { SettingsContext } from "../../context/SettingsContext";
 import styles from "./Modal.module.scss";
 
 const Modal = () => {
-	const { modalShow, setModalShow, modalContent } =
+	const { modalShow, setModalShow, modalContent, theme } =
 		useContext(SettingsContext);
 
-	const modalClass = modalShow ? styles.Wrapper : styles.Wrapper__Invis;
+	const wrapperClass = modalShow ? styles.Wrapper : styles.Wrapper__Invis;
 
 	return (
-		<div className={modalClass} onClick={() => setModalShow(false)}>
-			<div className={styles.Box}>{modalContent}</div>
+		<div className={wrapperClass} onClick={() => setModalShow(false)}>
+			<div
+				className={styles[`Box__${theme}`]}
+				onClick={(e) => e.stopPropagation()}
+			>
+				{modalContent}
+			</div>
 		</div>
 	);
 };
