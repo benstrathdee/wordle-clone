bd76477990d6960dd7e2c18b637e2cd5fa46db7a
Ben Strathdee <ben.strathdee@live.com.au>
2022-04-22 17:59:32 +1000

    moved some settings state, effect from Home

--- src/context/SettingsContext.jsx
+++ src/context/SettingsContext.jsx
@@ -1,11 +1,19 @@
-import { createContext, useState } from "react";
-import { getCookie } from "../functions/utilities";
+import { createContext, useState, useEffect } from "react";
+import { getCookie, setCookie } from "../functions/utilities";
 
 export const SettingsContext = createContext();
 
 export const SettingsProvider = ({ children }) => {
 	const [modalShow, setModalShow] = useState(false);
 	const [modalContent, setModalContent] = useState(null);
+
+	const [theme, setTheme] = useState(
+		localStorage.theme ? getCookie("theme") : "Light"
+	);
+
+	useEffect(() => {
+		setCookie("theme", theme);
+	}, [theme]);
 
 	const settingsTemplate = {};
 	const [settings, setSettings] = useState(
@@ -19,6 +27,8 @@ export const SettingsProvider = ({ children }) => {
 		setModalShow,
 		modalContent,
 		setModalContent,
+		theme,
+		setTheme,
 	};
 	return (
 		<SettingsContext.Provider value={data}>
