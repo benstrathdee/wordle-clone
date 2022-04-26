8a60afe342f6e174c96afaeb7a604c671bad5d35
Ben Strathdee <ben.strathdee@live.com.au>
2022-04-26 17:13:20 +1000

    moved datecode function

--- src/functions/grid.js
+++
@@ -1,9 +0,0 @@
-// Used to generate a number from the current date
-// used to determine the day's word
-export const getDateCode = () => {
-	const today = new Date();
-	const date = today.getDate();
-	const month = today.getMonth();
-	const year = today.getFullYear().toString().slice(2, 4);
-	return ((date + month + 1) * year * month).toString();
-};
--- src/functions/utilities.js
+++ src/functions/utilities.js
@@ -5,3 +5,11 @@ export const setCookie = (name, value) => {
 export const getCookie = (name) => {
 	return JSON.parse(localStorage.getItem(name));
 };
+
+export const getDateCode = () => {
+	const today = new Date();
+	const date = today.getDate();
+	const month = today.getMonth();
+	const year = today.getFullYear().toString().slice(2, 4);
+	return ((date + month + 1) * year * month).toString();
+};
